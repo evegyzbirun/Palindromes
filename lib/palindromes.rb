eb159c7390eaf441f6f151a7d8cb736af607a501
evegyzbirun <zbirunzbirun@gmail.com>
2022-09-06 20:03:40 -0700

    small fix.

--- lib/palindromes.rb
+++ lib/palindromes.rb
@@ -1,25 +1,26 @@
-# 1881 loop backwards over this and build a new string of 1881 and the compare the two. if ==? yes = palendrome or no
 
-#kayak.is_palindrome?()
-#[k,a,y,a,k]
-
-def is_palindrome?
-  test_subject = "keyak"
-
-  split_array = test_subject.split(//)
-  new_array = []
-  i = -1
-  while (i >= ((split_array.length - (split_array.length*2))))
-    new_array.push(split_array[i])
-    i -= 1
+class Palindrome
+  def initialize(word)
+    @word = word
   end
-  if (split_array == new_array)
-    puts "YOU GOT IT MY GREAT FRIEND!"
-    return true
-  else 
-    puts "HEY NOOOOOOOOOO THAT'S NOT IT!"
-    return false
-  end
-end 
 
-#((split_array.length - (split_array.length*2)))
+  def is_palindrome?
+    string_remove = @word.gsub(/[^A-Za-z0-9]/, '').downcase
+    puts string_remove
+
+    split_array = string_remove.split(//)
+    new_array = []
+    i = -1
+    while (i >= ((split_array.length - (split_array.length*2))))
+      new_array.push(split_array[i])
+      i -= 1
+    end
+    if (split_array == new_array)
+      puts "YOU GOT IT MY GREAT FRIEND!"
+      return true
+    else 
+      puts "HEY NOOOOOOOOOO THAT'S NOT IT!"
+      return false
+    end
+  end
+end
--- palindromes_script.rb
+++ palindromes_script.rb
@@ -1,6 +1,12 @@
 #!/usr/bin/env ruby
 require 'pry'
 require ('./lib/palindromes.rb')
+puts "Give me your best Palindrome for me to test:"
+test_pal = gets.chomp
+##test_pal2 = gets.chomp
+my_pal = Palindrome.new(test_pal)
+##my_pal2 = Palindrome.new(test_pal2)
+##binding.pry
+my_pal.is_palindrome?
+##my_pal2.
 
-test_subject = "kayak"
-test_subject.is_palindrome?
