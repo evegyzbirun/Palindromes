84f71876e241cccc359646957b6b613193664dea
evegyzbirun <zbirunzbirun@gmail.com>
2022-09-06 20:31:34 -0700

    small fix

--- palindromes_script.rb
+++ palindromes_script.rb
@@ -7,10 +7,10 @@ file_data = file.readlines.map(&:chomp)
 ##test_pal2 = gets.chomp
 
 pal_check_array = file_data[0].to_s.split(' ')
-pal_check_array.each do |n|
-  my_pal = Palindrome.new(n)
+pal_check_array.each do |element|
+  my_pal = Palindrome.new(element)
   if my_pal.is_palindrome?
-    puts "The palindrome is: #{n}"
+    puts "The palindrome is: #{element}"
   end
 end
 ##my_pal2 = Palindrome.new(test_pal2)
