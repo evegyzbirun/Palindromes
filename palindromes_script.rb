333494129b187654a288e286169b6a45c345fb4e
evegyzbirun <zbirunzbirun@gmail.com>
2022-09-06 20:41:06 -0700

    famm fix

--- palindromes_script.rb
+++ palindromes_script.rb
@@ -2,18 +2,22 @@
 require 'pry'
 require ('./lib/palindromes.rb')
 # puts "Give me your best Palindrome for me to test:"
-file = File.open("./palindrome.txt")
-file_data = file.readlines.map(&:chomp)
+# file = File.open("./palindrome.txt")
+file_data = File.read("./palindrome.txt").split
 ##test_pal2 = gets.chomp
 
-pal_check_array = file_data[0].to_s.split(' ')
-pal_check_array.each do |element|
-  my_pal = Palindrome.new(element)
-  if my_pal.is_palindrome?
-    puts "The palindrome is: #{element}"
+file_data.each do |n|
+pal_check_array = n.to_s.split(" ")
+  pal_check_array.each do |element|
+    if (element.length > 2) 
+      my_pal = Palindrome.new(element)
+      if my_pal.is_palindrome?
+        puts "The palindrome is: #{element}"
+      end
+    end
   end
 end
 ##my_pal2 = Palindrome.new(test_pal2)
-file_data = file.close
+# file_data = file.close
 ##my_pal2.
 
